b640ca5758ea778a05c21e0a680ef354175a2b1e
Siti Kaamiliaa Hasnaa <mila.kamiliah@yahoo.com>
2020-03-09 11:26:04 +0700

    first commit initiating repository

---
+++ src/test/java/bta/hris/HrisApplicationTests.java
@@ -0,0 +1,16 @@
+package bta.hris;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HrisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
