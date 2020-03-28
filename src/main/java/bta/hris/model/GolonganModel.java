cfc887dc2d14bf91775ec2aa0c616b208847de41
unknown <sandika.prangga@gmail.com>
2020-03-28 23:46:29 +0700

    add default value for isActive in GolonganModel

--- src/main/java/bta/hris/model/GolonganModel.java
+++ src/main/java/bta/hris/model/GolonganModel.java
@@ -25,7 +25,7 @@ public class GolonganModel implements Serializable {
     private Float pajak;
 
     @NotNull
-    @Column(name = "is_active", nullable = false, columnDefinition = "boolean default 'true'")
+    @Column(name = "is_active", nullable = false, columnDefinition = "boolean default true")
     private boolean isActive;
 
     @OneToMany(mappedBy = "golongan", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
