17acab4998cb380ebdfb395b642bbf56109dac2a
Dinda Mutiara Qur'ani Putri <dindaputri99@gmail.com>
2020-05-09 00:20:49 +0700

    daftar-tambah-ubah-hapus-detail paket soal

---
+++ src/main/java/bta/hris/service/PaketSoalService.java
@@ -0,0 +1,14 @@
+package bta.hris.service;
+
+import bta.hris.model.PaketSoalModel;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PaketSoalService {
+    List<PaketSoalModel> getAllPaketsoal();
+    PaketSoalModel addPaketSoal(PaketSoalModel paketSoal);
+    Optional<PaketSoalModel> getPaketSoalByIdPaket (Long idPaket);
+    PaketSoalModel updatePaketSoal(PaketSoalModel paketSoal);
+    PaketSoalModel deletePaketSoal(PaketSoalModel paketSoal);
+}
---
+++ src/main/java/bta/hris/service/PaketSoalServiceImpl.java
@@ -0,0 +1,59 @@
+package bta.hris.service;
+
+import bta.hris.model.PaketSoalModel;
+import bta.hris.repository.PaketSoalDB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class PaketSoalServiceImpl implements PaketSoalService{
+    @Autowired
+    private PaketSoalDB paketSoalDB;
+
+    @Override
+    public List<PaketSoalModel> getAllPaketsoal() {
+        return paketSoalDB.findAll();
+    }
+
+    @Override
+    public PaketSoalModel addPaketSoal(PaketSoalModel paketSoal) {
+        paketSoal.setActive(true);
+        paketSoalDB.save(paketSoal);
+        return paketSoal;
+    }
+
+    @Override
+    public Optional<PaketSoalModel> getPaketSoalByIdPaket(Long idPaket) {
+        return paketSoalDB.findByIdPaket(idPaket);
+    }
+
+    @Override
+    public PaketSoalModel updatePaketSoal(PaketSoalModel paketSoal) {
+        PaketSoalModel newPaket = paketSoalDB.findByIdPaket(paketSoal.getIdPaket()).get();
+
+        try{
+            newPaket.setNama(paketSoal.getNama());
+            newPaket.setMataPelajaran(paketSoal.getMataPelajaran());
+
+            paketSoalDB.save(newPaket);
+            return newPaket;
+        }
+        catch (NullPointerException nullException){
+            return null;
+        }
+    }
+
+    @Override
+    public PaketSoalModel deletePaketSoal(PaketSoalModel paketSoal) {
+        PaketSoalModel target = paketSoalDB.findByIdPaket(paketSoal.getIdPaket()).get();
+        target.setActive(false);
+        paketSoalDB.save(target);
+
+        return target;
+    }
+}
