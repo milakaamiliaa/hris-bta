b9cbeeeb65df255b268019565bc350db43cacb8d
unknown <sandika.prangga@gmail.com>
2020-05-19 05:40:36 +0700

    add @Transactional to UserDetailsServiceImpl.java

--- src/main/java/bta/hris/security/UserDetailsServiceImpl.java
+++ src/main/java/bta/hris/security/UserDetailsServiceImpl.java
@@ -11,11 +11,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 @Service("userDetailsService")
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserDB userDb;
