ae1f7be18fb94490212937b5f544ba712843f71f
unknown <sandika.prangga@gmail.com>
2020-05-19 16:42:13 +0700

    enable randomization of answer choices for each CalonPengajar

--- src/main/java/bta/hris/controller/TesController.java
+++ src/main/java/bta/hris/controller/TesController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -101,6 +102,7 @@ public class TesController {
 
             ss.setPaketSoal(submittedPaketSoal);
             ss.setPertanyaan(s.getPertanyaan());
+            Collections.shuffle(submittedJawaban);
             ss.setListJawaban(submittedJawaban);
 
             submittedSoal.add(ss);
