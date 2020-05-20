15dbf56f7289dda543e6e3881f4c679accaae9d1
yudharp <yudhaananda.rp@gmail.com>
2020-05-21 01:27:22 +0700

    fixing berada staf cabang

--- src/main/java/bta/hris/controller/PageController.java
+++ src/main/java/bta/hris/controller/PageController.java
@@ -112,13 +112,6 @@ public class PageController {
                 UserModel pegawaiAtIndex = pegawaiList.get(index);
                 pegawaiListSorted.add(pegawaiAtIndex);
 
-                model.addAttribute("pegawaiList", pegawaiListSorted);
-                model.addAttribute("totalSesiPegawai", totalsesiPegawaiSorted);
-                model.addAttribute("cabangData", cabangData);
-                model.addAttribute("totalPayroll", totalPayroll);
-                model.addAttribute("gajiModel", gajiModel);
-                model.addAttribute("userModel", userModel);
-
                 // ==== Keperluan Models Attribute untuk grafik ====
                     // List CabangData yang akan dipakai (5 bulan ke belakang dari bulan sekarang).
                 List<CabangDataModel> listCabangData = cabangDataService.getCabangDatasForXPeriodBeforeNow(5, cabangModel);
@@ -138,15 +131,16 @@ public class PageController {
                 periodeString = periodeString.substring(0, periodeString.length()-1);
                 rasioString = rasioString.substring(0, rasioString.length()-1);
 
-                if (listCabangData.size()<5){
+               /* if (listCabangData.size()<5){
                     model.addAttribute("dataNotEnough", "Data belum mencukupi untuk memunculkan grafik " +
                             "rasio efisiensi cabang");
-                }
-                else {
-                    model.addAttribute("dataEnough", "Data mencukupi untuk memunculkan grafik " +
-                            "rasio efisiensi cabang");
-                }
-
+                }*/
+                model.addAttribute("pegawaiList", pegawaiListSorted);
+                model.addAttribute("totalSesiPegawai", totalsesiPegawaiSorted);
+                model.addAttribute("cabangData", cabangData);
+                model.addAttribute("totalPayroll", totalPayroll);
+                model.addAttribute("gajiModel", gajiModel);
+                model.addAttribute("userModel", userModel);
                 model.addAttribute("periodeString", periodeString);
                 model.addAttribute("rasioString", rasioString);
 
